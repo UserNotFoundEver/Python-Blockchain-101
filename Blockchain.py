e0c10091b97630557b9c730e60ab39bb965997ac
Sergio <sergiobluez@gmail.com>
2018-05-02 14:04:35 -0700

    Updated blockchain python example

--- Blockchain.py
+++ Blockchain.py
@@ -5,26 +5,53 @@ blockchain = []
 
 # [-1] index to get the last value in the blockchain/list
 def get_last_blockchain_value():
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
 
-def add_value(transaction_amount, last_transaction=[1]):
+def add_transaction(transaction_amount, last_transaction=[1]):
+    if last_transaction == None:
+        last_transaction = [1]
     blockchain.append([last_transaction, transaction_amount])
 
 
-def get_user_input():
+def get_transaction_value():
     user_input = float(input('Your transaction amount: '))
     return user_input
 
 
-tx_amount = get_user_input()
-add_value(tx_amount)
-
-tx_amount = get_user_input()
-add_value(last_transaction=get_last_blockchain_value(), transaction_amount=tx_amount)
-
-tx_amount = get_user_input()
-add_value(tx_amount, get_last_blockchain_value())
+def get_user_choice():
+    user_input = input('Your choice ')
+    return user_input
 
 
-print(blockchain)
+def print_blockchain_elements():
+    #  Output blockchain list
+    for block in blockchain:
+        print('Output Block')
+        print(block)
+
+
+# Get first transaction input
+tx_amount = get_transaction_value()
+add_transaction(tx_amount)
+
+# Add as many blocks as you want
+while True:
+    print('Please choose ')
+    print('1: Add a new transaction value ')
+    print('2: Output the blockchain blocks ')
+    print('2: Quit ')
+    user_choice = get_user_choice()
+    if user_choice == '1':
+        txt_amount = get_transaction_value()
+        add_transaction(tx_amount, get_last_blockchain_value())
+    elif user_choice == '2':
+        print_blockchain_elements()
+    elif user_choice == 'q':
+        break
+    else:
+        print('Input was invalid, please pick a value from the list!')
+
+print('Done!!')
