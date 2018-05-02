70aea572839cea06343302240dc5457b40eba433
Sergio <sergiobluez@gmail.com>
2018-05-02 13:11:22 -0700

    blockchain.py
    Testing blockchain basics with Python.

---
+++ Blockchain.py
@@ -0,0 +1,30 @@
+"""let there be blockchain - experiments"""
+
+blockchain = []
+
+
+# [-1] index to get the last value in the blockchain/list
+def get_last_blockchain_value():
+    return blockchain[-1]
+
+
+def add_value(transaction_amount, last_transaction=[1]):
+    blockchain.append([last_transaction, transaction_amount])
+
+
+def get_user_input():
+    user_input = float(input('Your transaction amount: '))
+    return user_input
+
+
+tx_amount = get_user_input()
+add_value(tx_amount)
+
+tx_amount = get_user_input()
+add_value(last_transaction=get_last_blockchain_value(), transaction_amount=tx_amount)
+
+tx_amount = get_user_input()
+add_value(tx_amount, get_last_blockchain_value())
+
+
+print(blockchain)
